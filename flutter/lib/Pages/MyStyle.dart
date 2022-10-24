74d09f8832a57c003c880a4791c6eddf9c04769d
sweet62 <maks_shuyanov@mail.ru>
2022-10-24 19:23:50 +0300

    Add
    test

---
+++ flutter/lib/Pages/MyStyle.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController();
+    return PageView(
+      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+      /// Use [Axis.vertical] to scroll vertically.
+      controller: controller,
+      children: const <Widget>[
+        Align(alignment: Alignment(-0.50, -0.900),
+              child: Text(
+                  "Фио",
+                  style: TextStyle(fontSize: 32)),
+        ),
+        Center(
+          child: Text('Second Page'),
+        ),
+        Center(
+          child: Text('Third Page'),
+        ),
+      ],
+    );
+  }
+}
---
+++ flutter/lib/PersonalDates.dart
@@ -0,0 +1,12 @@
+class DatePS{
+  String Name;
+  String Surname;
+  String other;
+
+
+  DatePS(
+    this.Name,
+    this.Surname,
+    this.other
+  );
+}
