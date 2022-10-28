02e68cf9d21d480b1ed68e6d4993c6ea6f13b301
Sasha <h2so4corotcovo@gmail.com>
2022-10-28 14:21:44 +0300

    добавление общего фона на все страницы

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: const Color.fromARGB(255, 210, 227, 246)),
       home: MyStatefulWidget(),
       debugShowCheckedModeBanner: false,
     );
