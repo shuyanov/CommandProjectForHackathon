3c30c7257089e8ead2f95b1758704a39aff36c31
Oleg Kulichkov <56484404+HubOl01@users.noreply.github.com>
2022-10-28 19:59:46 +0300

    Revert "chats"

--- flutter/lib/Chats/Data/Admin.dart
+++
@@ -1,4 +0,0 @@
-String myId = 'b9YFGIpJjzGpJftfLnS6';
-String myUsername = 'user1';
-String myDecription = 'citizen';
-String myUrlAvatar = 'https://avatars.mds.yandex.net/i?id=e3f19061fc32953419cea5c5a238f86c_l-5309503-images-thumbs&n=13';
